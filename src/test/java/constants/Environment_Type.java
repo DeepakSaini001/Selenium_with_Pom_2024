18c8f29c59233b66b84be4195d9c6cb58978b659
Deepak <Deepak.saini2106@gmail.com>
2024-06-24 16:39:27 +0530

    env specific config file QA/CT

---
+++ src/test/java/constants/Environment_Type.java
@@ -0,0 +1,9 @@
+package constants;
+
+public enum Environment_Type {
+	
+	CT,
+	QA
+	
+
+}
--- src/test/java/org/utils/ConfigLoader.java
+++ src/test/java/org/utils/ConfigLoader.java
@@ -2,6 +2,8 @@ package org.utils;
 
 import java.util.Properties;
 
+import constants.Environment_Type;
+
 public class ConfigLoader {
 
 	private final Properties properties;
@@ -12,11 +14,24 @@ public class ConfigLoader {
 	// SingleTon design pattern--- means it cannot be access outside of this class
 
 	// Private constructor to prevent instantiation --- SingleTon Design patter
-	// private---- constructor cannot be access outside of this class and only 1 instance can be created 
+	// private---- constructor cannot be access outside of this class and only 1
+	// instance can be created
 	private ConfigLoader() {
+		// String environment = System.getProperty("environment", "CT");
+		String environment = System.getProperty("environment", String.valueOf(Environment_Type.CT));
+		switch (Environment_Type.valueOf(environment)) {
+		case CT:
+			properties = PropertyUnitls.propertyLoader(
+					"//Users//deepaksaini//eclipse-workspace//Selenium_DS_POM_May2024//src//test//resources//config_CT.properties");
+			break;
+		case QA:
 
-		properties = PropertyUnitls.propertyLoader(
-				"//Users//deepaksaini//eclipse-workspace//Selenium_DS_POM_May2024//src//test//resources//config_CT.properties");
+			properties = PropertyUnitls.propertyLoader(
+					"//Users//deepaksaini//eclipse-workspace//Selenium_DS_POM_May2024//src//test//resources//config_QA.properties");
+			break;
+		default:
+			throw new IllegalStateException("Invalid Environment type" + " : " + environment);
+		}
 	}
 
 	// Public method to provide access to the singleton instance
@@ -30,8 +45,8 @@ public class ConfigLoader {
 		 * in If condition---if configLoader is null, that means there is no instance of
 		 * this class, then we are creating a new instance of this class
 		 * 
-		 * and when this getInstance(), is called again it will not be null then no new instance
-		 * object will be created
+		 * and when this getInstance(), is called again it will not be null then no new
+		 * instance object will be created
 		 */
 		return configLoader;
 	}
