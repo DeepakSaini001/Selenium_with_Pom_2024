fea1056fcf3528bd05c9629bd5b4874a0f080c4e
Deepak <Deepak.saini2106@gmail.com>
2024-07-04 00:59:59 +0530

    new api changes

--- src/test/java/api/actions/SignUpAPI.java
+++ src/test/java/api/actions/SignUpAPI.java
@@ -1,14 +1,17 @@
 package api.actions;
 
 import io.restassured.http.Cookies;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-
 import static io.restassured.RestAssured.given;
-
+import java.util.HashMap;
+import org.Objects.User;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+
 import org.utils.ConfigLoader;
 
 public class SignUpAPI {
@@ -52,4 +55,33 @@ public class SignUpAPI {
 		}
 			return response;	
 	}
+	public Response register(User user){
+        Cookies cookies = new Cookies();
+        Header header = new Header("content-type", "application/x-www-form-urlencoded");
+        Headers headers = new Headers(header);
+        HashMap<String, String> formParams = new HashMap<>();
+        formParams.put("username", user.getUsername());
+        formParams.put("email", user.getEmail());
+        formParams.put("password", user.getPassword());
+        formParams.put("woocommerce-register-nonce", fetchRegisterNonceValueUsingJsoup());
+        formParams.put("register", "Register");
+
+        Response response = given().
+                baseUri(ConfigLoader.getInstance().getBaseUrl()).
+                headers(headers).
+                formParams(formParams).
+                cookies(cookies).
+                log().all().
+                when().
+        post("/account").
+                then().
+                log().all().
+                extract().
+                response();
+        if(response.getStatusCode() != 302){
+            throw new RuntimeException("Failed to register the account, HTTP Status Code: " + response.getStatusCode());
+        }
+        this.cookies = response.getDetailedCookies();
+        return response;
+    }
 }
--- src/test/java/test/d.java
+++ src/test/java/test/d.java
@@ -7,7 +7,7 @@ public class d {
 	public static void main(String[] args) {
 
 		SignUpAPI a = new SignUpAPI();
-		System.out.println("Fetching the register value using Groovy"+" : "+a.fetchRegisterNonceValueUsingGroovy());
-		System.out.println("Fetching the register value using JSoup"+" : "+a.fetchRegisterNonceValueUsingJsoup());
+	//	System.out.println("Fetching the register value using Groovy"+" : "+a.fetchRegisterNonceValueUsingGroovy());
+	//	System.out.println("Fetching the register value using JSoup"+" : "+a.fetchRegisterNonceValueUsingJsoup());
 	}
 }
